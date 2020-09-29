257b7fad472d5d694a8b53d4c3f3623675a7eedd
Johan Petrikovsky <johan.petrikovsky@sigfox.com>
2020-09-29 15:51:28 +0200

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+# Test Sentry
+
+Simple React App to test Sentry
+
 ## Available Scripts
 
 In the project directory, you can run:
@@ -66,3 +70,5 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+# test-sentry
--- src/App.js
+++ src/App.js
@@ -3,18 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const RELEASE = '0.1.0';
+
+  Sentry.init({
+    dsn:
+      'https://8d6259633fb64e91aaa9b6ad76a422d7@o451965.ingest.sentry.io/5438682',
+    release: RELEASE,
+  });
+
+  const handleClick = () => {
+    try {
+      throw new Error('Caught');
+    } catch (err) {
+      console.error(error);
+      Sentry.captureException(err);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <button onClick={handleClick}>Error</button>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
